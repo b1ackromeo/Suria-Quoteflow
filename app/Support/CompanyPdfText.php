dfdefa375ae730d28a2e03e8d159e2d13faec8b4
b1ackromeo <120737381+b1ackromeo@users.noreply.github.com>
2026-05-20 22:16:49 +0800

    Use neutral tax registration wording in PDFs
    Render company tax registration with a globally neutral label in generated PDFs.

--- app/Support/CompanyPdfText.php
+++ app/Support/CompanyPdfText.php
@@ -25,7 +25,7 @@ class CompanyPdfText
             return $html;
         }
 
-        $taxRegistrationLine = e($companyProfile->taxLabel().' Reg. No. '.$companyProfile->tax_registration_number);
+        $taxRegistrationLine = e('Tax Registration No. '.$companyProfile->tax_registration_number);
 
         return (string) preg_replace(
             '/(<div class="company-meta">.*?)(<\/div>)/s',
